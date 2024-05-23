9da1b46a0d0326c07ee7de45a324e9feccba8094
dolianich <69437697+dolianich@users.noreply.github.com>
2024-05-23 15:49:38 +0300

    Created a Button component

---
+++ app/components/Button/Button.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styles from './Button.module.css';
+
+type ButtonType = 'button' | 'submit' | 'reset' | undefined;
+type ButtonVariant = 'primary' | 'secondary' | 'tertiary' | 'quaternary';
+
+interface Props {
+  children: React.ReactNode;
+  handleClick?: () => void;
+  title: string;
+  disabled?: boolean;
+  type?: ButtonType;
+  variant?: ButtonVariant;
+}
+
+const Button = ({
+  children,
+  handleClick,
+  title,
+  disabled,
+  type,
+  variant,
+}: Props) => {
+  let buttonClass = variant ? styles[variant] : styles.primary;
+  return (
+    <button
+      onClick={handleClick}
+      className={buttonClass}
+      type={type}
+      title={title}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
