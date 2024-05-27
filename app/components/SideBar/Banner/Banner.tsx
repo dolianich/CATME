8d0ba0af94770564009570b837807374ab5c3160
dolianich <69437697+dolianich@users.noreply.github.com>
2024-05-27 16:31:23 +0300

    Refactor: changed SideBar component styles

--- app/components/SideBar/Banner/Banner.tsx
+++ app/components/SideBar/Banner/Banner.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import Image from 'next/image';
-import nft from './src/FreeNFT.png'
-import styles from './Banner.module.css'
+import nft from './src/FreeNFT.png';
+import styles from './Banner.module.css';
 import Button from '../../Button/Button';
 
 const Banner = () => {
   return (
     <div className={styles.banner}>
-      <Image src={nft} alt="NFT" width={224} height={224} className={styles.image}/>
-      <Button  title={'CLAIM NFT'} variant='quaternary'>CLAIM NFT</Button>
+      <Image
+        src={nft}
+        alt="NFT"
+        width={200}
+        height={200}
+        className={styles.image}
+      />
+      <Button title={'CLAIM NFT'} variant="quaternary">
+        CLAIM NFT
+      </Button>
     </div>
   );
 };
