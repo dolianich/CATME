7b32884bb6518ad36e380d602ebdd2ca25d9e28a
dolianich <69437697+dolianich@users.noreply.github.com>
2024-06-03 13:42:02 +0300

    Styled the Dialog component

--- app/components/SideBar/Banner/Dialog/Dialog.tsx
+++ app/components/SideBar/Banner/Dialog/Dialog.tsx
@@ -2,21 +2,24 @@ import React, { ReactNode } from 'react';
 import styles from './Dialog.module.css';
 import { forwardRef } from 'react';
 
-interface Props{
-    children: React.ReactNode;
-    toggleDialog: () => void;
+interface Props {
+  children: React.ReactNode;
+  toggleDialog: () => void;
 }
 
-const Dialog = forwardRef<HTMLDialogElement, Props>(({children, toggleDialog}, ref) => {
-  return (
-    <dialog
-      ref={ref}
-      onClick={(e) => e.currentTarget === e.target && toggleDialog()}
-    >
-      {children}
-      <button onClick={toggleDialog}>Close</button>
-    </dialog>
-  );
-});
+const Dialog = forwardRef<HTMLDialogElement, Props>(
+  ({ children, toggleDialog }, ref) => {
+    return (
+      <dialog
+        className={styles.dialog}
+        ref={ref}
+        onClick={(e) => e.currentTarget === e.target && toggleDialog()}
+      >
+        {children}
+        {/*<button onClick={toggleDialog}>Close</button>*/}
+      </dialog>
+    );
+  }
+);
 
 export default Dialog;
