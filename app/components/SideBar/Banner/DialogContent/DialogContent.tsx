662001d0c7e0cbe3a122f533f0e4ef0b80c12787
dolianich <69437697+dolianich@users.noreply.github.com>
2024-06-03 17:11:32 +0300

    Filled the Dialog Content component

--- app/components/SideBar/Banner/DialogContent/DialogContent.tsx
+++ app/components/SideBar/Banner/DialogContent/DialogContent.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import styles from './DialogContent.module.css';
+import Button from '@/app/components/Button/Button';
+import Image from 'next/image';
+import nft from '../src/FreeNFT.png';
 
 const DialogContent = () => {
   return (
-    <div>
-      <h1>NFT Claim</h1>
+    <div className={styles.dialogContainer}>
+      <Image
+        src={nft}
+        alt="NFT"
+        width={360}
+        height={360}
+        quality={100}
+        className={styles.nft}
+      />
+      <h1>WELCOME BADGE</h1>
+      <p>Welcome to the future of WEB3 gaming!<br />Your CATME membership starts here,<br />unlocking gaming adventures</p>
+      <Button variant="quaternary" title="CLAIM NFT">
+        CLAIM NFT
+      </Button>
     </div>
   );
 };
