d5cf16df2ebacd8bce5d976d2d4316799eb995e7
dolianich <69437697+dolianich@users.noreply.github.com>
2024-06-09 15:31:12 +0300

    Moved gameData to a separate file

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -2,44 +2,9 @@
 import styles from './page.module.css';
 import Heading from '../components/Heading/Heading';
 import GameCard from '../components/GameCard/GameCard';
-import hollowKnight from '../src/gamesImages/hollowKnight.png';
-import cuphead from '../src/gamesImages/cupHead.jpeg';
-import zelda from '../src/gamesImages/zelda.png';
-import superMario from '../src/gamesImages/super-mario.png';
+import {gamesData} from '../gameData/gamesData';
 
-const games = [
-  {
-    id: '1',
-    title: 'Hollow Knight',
-    category: 'arcade',
-    cover: hollowKnight,
-    price: 'free',
-    shortDescription: 'This is one line description of this game',
-  },
-  {
-    id: '2',
-    title: 'Cuphead',
-    category: 'MINI-GAME',
-    cover: cuphead,
-    price: 'free',
-  },
-  { id: '3', title: 'Zelda', category: 'DEGEN', cover: zelda, price: 'free' },
-  {
-    id: '4',
-    title: 'Super Mario',
-    category: 'P2E',
-    cover: superMario,
-    price: 'free',
-  },
-  { id: '6', title: 'Unknown', category: 'P2E', price: 'free' },
-  { id: '7', title: 'Unknown', category: 'P2E', price: 'free' },
-  { id: '8', title: 'Unknown', category: 'P2E', price: 'free' },
-  { id: '9', title: 'Unknown', category: 'P2E', price: 'free' },
-  { id: '10', title: 'Unknown', category: 'P2E', price: 'free' },
-  { id: '11', title: 'Unknown', category: 'P2E', price: 'free' },
-  { id: '12', title: 'Unknown', category: 'P2E', price: 'free' },
-  { id: '13', title: 'Unknown', category: 'P2E', price: 'free' },
-];
+
 
 const play = (gameTitle: string) =>
   console.log('Play a ' + gameTitle + ' game');
@@ -51,7 +16,7 @@ export default function Home() {
     <div>
       <Heading>Recommended Games</Heading>
       <div className={styles.cardsSection}>
-        {games.map((game) => (
+        {gamesData.map((game) => (
           <GameCard
             key={game.id}
             infoClick={() => getInfo(game.title)}
---
+++ app/gameData/gamesData.ts
@@ -0,0 +1,38 @@
+import hollowKnight from '../src/gamesImages/hollowKnight.png';
+import cuphead from '../src/gamesImages/cupHead.jpeg';
+import zelda from '../src/gamesImages/zelda.png';
+import superMario from '../src/gamesImages/super-mario.png'; 
+ 
+ export const gamesData = [
+    {
+      id: '1',
+      title: 'Hollow Knight',
+      category: 'arcade',
+      cover: hollowKnight,
+      price: 'free',
+      shortDescription: 'This is one line description of this game',
+    },
+    {
+      id: '2',
+      title: 'Cuphead',
+      category: 'MINI-GAME',
+      cover: cuphead,
+      price: 'free',
+    },
+    { id: '3', title: 'Zelda', category: 'DEGEN', cover: zelda, price: 'free' },
+    {
+      id: '4',
+      title: 'Super Mario',
+      category: 'P2E',
+      cover: superMario,
+      price: 'free',
+    },
+    { id: '6', title: 'Another game', category: 'P2E', price: 'free' },
+    { id: '7', title: 'Unknown', category: 'P2E', price: 'free' },
+    { id: '8', title: 'Unknown', category: 'P2E', price: 'free' },
+    { id: '9', title: 'Unknown', category: 'P2E', price: 'free' },
+    { id: '10', title: 'Unknown', category: 'P2E', price: 'free' },
+    { id: '11', title: 'Unknown', category: 'P2E', price: 'free' },
+    { id: '12', title: 'Unknown', category: 'P2E', price: 'free' },
+    { id: '13', title: 'Unknown', category: 'P2E', price: 'free' },
+  ];
