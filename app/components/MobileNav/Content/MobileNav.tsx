bf844fd75960b4119df3b02a61c2b58fc7534b3c
dolianich <69437697+dolianich@users.noreply.github.com>
2024-06-13 12:34:42 +0300

    Made the Flappy Bird game playable on small screens

--- app/components/MobileNav/Content/MobileNav.tsx
+++ app/components/MobileNav/Content/MobileNav.tsx
@@ -68,7 +68,7 @@ const MobileNav = ({ onClick }: Props) => {
             href={button.href}
             size={button.size}
             color={`${
-              currentPath.startsWith(button.href) ? '#FF5756' : '#85868B'
+              currentPath.startsWith(button.href) ? '#F0F1FF' : '#85868B'
             }`}
             weight={`${
               currentPath.startsWith(button.href) ? 'fill' : 'regular'
