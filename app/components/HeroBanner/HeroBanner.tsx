cee31b53605ff1f4bdf307afb9340b2096fc26fc
dolianich <69437697+dolianich@users.noreply.github.com>
2024-06-17 15:21:13 +0300

    Started HeroBanner component and uploaded animation file

---
+++ app/components/HeroBanner/HeroBanner.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './HeroBanner.module.css';
+
+const HeroBanner = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HeroBanner
