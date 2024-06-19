43ada2b1fd0bb8dcc7b89f1c3d537aa24afc6f28
dolianich <69437697+dolianich@users.noreply.github.com>
2024-06-19 21:47:43 +0300

    Strted the testing character creation page

--- app/games/dilectus/page.tsx
+++ app/games/dilectus/page.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Heading from '@/app/components/Heading/Heading';
+import styles from './page.module.css';
 
 const Dilectus = () => {
   return (
-    <div>
-      
+    <div className={styles.wrap}>
+      <Heading>DILECTUS</Heading>
+      <div className={styles.alpaca}>
+        <div className={styles.left}>
+          <div className={styles.alpacaArt}></div>
+          <div className={styles.actions}> Download and Random</div>
+        </div>
+        <div className={styles.right}>
+          <div className={styles.rightTop}>
+            <h3>Accessories the Alpaca</h3>
+          </div>
+          <div className={styles.rightBottom}>
+            <h3>Styles</h3>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dilectus
+export default Dilectus;
