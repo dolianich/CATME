65ba49029dadd548df27ff3061b2ca71bcacce10
dolianich <69437697+dolianich@users.noreply.github.com>
2024-07-05 14:34:33 +0300

    Started styling the info component and added Delectus token image

--- app/games/dilectus/components/Info/Info.tsx
+++ app/games/dilectus/components/Info/Info.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+import styles from './Info.module.css';
+import { House, PuzzlePiece, Bone, Palette, Bed } from '@phosphor-icons/react';
+import Image from 'next/image';
+import token from '../../data/src/general/token.svg';
 
 const Info = () => {
   return (
-    <div>
-      Data
+    <div className={styles.wrap}>
+      <div className={styles.slot}>
+        <div className={styles.balanceIcon}>
+        <Image src={token} alt={'img'} className={styles.token} />
+        </div>
+        <div className={styles.balance}>999 999</div>
+      </div>
+      <div className={styles.slot}>
+        <div className={styles.balanceIcon}></div>
+        <div className={styles.balance}>999 999</div>
+      </div>
+      <div className={styles.slot}>
+        <div className={styles.balanceIcon}></div>
+        <div className={styles.balance}>999 999</div>
+      </div>
+      <div className={styles.slot}>
+        <div className={styles.balanceIcon}></div>
+        <div className={styles.balance}>999 999</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Info
+export default Info;
