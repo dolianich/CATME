8eed1305d1df73d1adb4f285f0fcf1f136483e97
dolianich <69437697+dolianich@users.noreply.github.com>
2024-07-05 14:46:21 +0300

    Added icons and styled the button

--- app/games/dilectus/components/Info/Info.tsx
+++ app/games/dilectus/components/Info/Info.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Info.module.css';
-import { House, PuzzlePiece, Bone, Palette, Bed } from '@phosphor-icons/react';
+import { Heart, Lightning } from '@phosphor-icons/react';
 import Image from 'next/image';
 import token from '../../data/src/general/token.svg';
 
@@ -9,21 +9,25 @@ const Info = () => {
     <div className={styles.wrap}>
       <div className={styles.slot}>
         <div className={styles.balanceIcon}>
-        <Image src={token} alt={'img'} className={styles.token} />
+          <Image src={token} alt={'img'} className={styles.token} />
         </div>
         <div className={styles.balance}>999 999</div>
       </div>
       <div className={styles.slot}>
-        <div className={styles.balanceIcon}></div>
+        <div className={styles.balanceIcon}>
+          <Heart size={36} weight="fill" />
+        </div>
         <div className={styles.balance}>999 999</div>
       </div>
       <div className={styles.slot}>
-        <div className={styles.balanceIcon}></div>
+        <div className={styles.balanceIcon}>
+          <Lightning size={36} weight="fill" />
+        </div>
         <div className={styles.balance}>999 999</div>
       </div>
       <div className={styles.slot}>
-        <div className={styles.balanceIcon}></div>
-        <div className={styles.balance}>999 999</div>
+        <div className={styles.questButton}>
+        </div>
       </div>
     </div>
   );
