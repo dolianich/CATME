581f5af97b9c12a549de7107dfc230008d632038
dolianich <69437697+dolianich@users.noreply.github.com>
2024-07-05 15:12:42 +0300

    Finished the Info component

--- app/games/dilectus/components/Info/Info.tsx
+++ app/games/dilectus/components/Info/Info.tsx
@@ -11,22 +11,25 @@ const Info = () => {
         <div className={styles.balanceIcon}>
           <Image src={token} alt={'img'} className={styles.token} />
         </div>
-        <div className={styles.balance}>999 999</div>
+        <div className={styles.balance}>
+          <p>999 999</p>
+        </div>
       </div>
       <div className={styles.slot}>
-        <div className={styles.balanceIcon}>
-          <Heart size={36} weight="fill" />
+        <div className={styles.heartIcon}>
+          <Heart size={36} weight="fill" color="#FFA6BD" />
         </div>
-        <div className={styles.balance}>999 999</div>
+        <div className={styles.health}></div>
       </div>
       <div className={styles.slot}>
-        <div className={styles.balanceIcon}>
-          <Lightning size={36} weight="fill" />
+        <div className={styles.energyIcon}>
+          <Lightning size={36} weight="fill" color="#C1A6FF" />
         </div>
-        <div className={styles.balance}>999 999</div>
+        <div className={styles.energy}></div>
       </div>
       <div className={styles.slot}>
         <div className={styles.questButton}>
+          <p>QUESTS</p>
         </div>
       </div>
     </div>
